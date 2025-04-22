38f42dd1e9eaba4771afc3857f9ac4db80eba103
Jared Heeringa <jaredheeringa@icloud.com>
2025-04-21 20:46:31 -0500

    Update server.sh

--- includes/server.sh
+++ includes/server.sh
@@ -21,7 +21,13 @@ function start_server() {
         send_start_notification
     fi
     es ">>> Starting the gameserver"
-    ./PalServer.sh "${START_OPTIONS[@]}"
+
+    # Check if we are using UE4SS and preload libUE4SS.so else start normal
+    if [[ "$UE4SS_ENABLED" == "true" ]]; then
+    	LD_PRELOAD=/palworld/Pal/Binaries/Linux/libUE4SS.so ./PalServer.sh "${START_OPTIONS[@]}"
+    else
+    	./PalServer.sh "${START_OPTIONS[@]}"
+    fi
 }
 
 function stop_server() {
